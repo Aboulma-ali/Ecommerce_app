d116fa17b162b19d683b67d0cef2cc598a1e87b1
Aboulma-ali <aboulmalia@gmail.com>
2025-07-30 05:23:50 +0000

    gestion de role avec package spatie/laravel-permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasRoles;
 
     protected $fillable = [
         'name',
@@ -26,6 +27,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    protected static function booted(): void
+    {
+        // Cette fonction sera appelée après la création d'un nouvel utilisateur
+        static::created(function ($user) {
+            // On lui assigne le rôle 'Client' par défaut
+            $user->assignRole('Client');
+        });
+    }
 
     // Relations
     public function addresses()
